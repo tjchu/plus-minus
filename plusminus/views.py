4acb676993acb7a439056560df62509864079217
Terry Chu <terrychu2002@gmail.com>
2018-08-30 14:03:06 -0700

    Removed commented out code

--- plusminus/views.py
+++ plusminus/views.py
@@ -10,26 +10,6 @@ def index(request):
     template = loader.get_template("plusminus/index.html")
     return HttpResponse(template.render())
 
-"""
-def get_stats(request, fga, fgm):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = StatsForm(request.POST, fga=fga, fgm=fgm)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            request.session['fga'] = form.fga
-            request.session['fgm'] = form.fgm
-            return HttpResponseRedirect('/results/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = StatsForm(fga=fga, fgm=fgm)
-"""
-
 def results(request): 
     name = request.POST.get('name')
     fgm = int(request.POST.get('fgm'))
